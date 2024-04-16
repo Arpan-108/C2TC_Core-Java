5aad94678c618dead6be3ed2e6502c758f5d953c
CC-55 <CC-55@172.16.14.93>
2024-04-16 09:00:50 +0530

    3rd commit

---
+++ TNSCoreJava/src/com/tnsif/daythree/Point.java
@@ -0,0 +1,17 @@
+package com.tnsif.daythree;
+
+public class Point {
+	private float x,y;
+	public Point() {
+		x = 0.0f;
+		y = 0.0f;
+	}
+	public Point(float value) {
+		x = value;
+		y = value;
+	}
+	public Point(float a, float b) {
+		x = a;
+		y = b;
+	}
+}
