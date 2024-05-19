e611585092a6d24f0bd28920085678dd7bfe1158
DELL <DELL@DESKTOP-PPR89CI>
2024-05-19 16:09:14 +0530

    12th commit

---
+++ TNSCoreJava/src/com/tnsif/asg2/AirFare.java
@@ -0,0 +1,7 @@
+package com.tnsif.asg2;
+
+public interface AirFare {
+	
+	double  calculateAmount();
+	
+}
--- TNSCoreJava/src/com/tnsif/dayeight/GreetDemo.java
+++ TNSCoreJava/src/com/tnsif/dayeight/GreetDemo.java
@@ -1,23 +1,23 @@
 package com.tnsif.dayeight;
 
-public class Demo {
+public class GreetDemo {
 
 	public static void main(String[] args) {
-		
+			
 		GreetImp obj=new GreetImp();
 		System.out.println(obj.sayHello("Arpan"));
-		
+			
 		GreetImp2 obj2=new GreetImp2();
 		System.out.println(obj2.sayHello("Rutvik"));
-		
-		
-		
+			
+			
+			
 		//Using Lambda Expression
 		IGreet iObj=(name)->"Hii "+ name;
 		System.out.println(iObj.sayHello("Rushi"));
-		
+			
 		//OR
-		
+			
 		iObj=(name)->{
 			return ("Good Morning "+name);
 		};
@@ -25,3 +25,5 @@ public class Demo {
 	}
 
 }
+
+
