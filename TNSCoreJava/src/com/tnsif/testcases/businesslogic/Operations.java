d031db2d17191bd0180caf7f99000bf4739c0bac
DELL <arpanshah848@gmail.com>
2024-06-26 16:37:16 +0530

    30.2th Commit

---
+++ TNSCoreJava/src/com/tnsif/testcases/businesslogic/Operations.java
@@ -0,0 +1,9 @@
+package com.tnsif.testcases.businesslogic;
+
+public class Operations {
+
+	public static int add(int a, int b) {
+		
+		return a+b;
+	}
+}
---
+++ TNSCoreJava/src/com/tnsif/testcases/testing/MyTestClass.java
@@ -0,0 +1,19 @@
+package com.tnsif.testcases.testing;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.tnsif.testcases.businesslogic.Operations;
+
+public class MyTestClass {
+
+	@Test
+	public void testAddition() {
+		
+		int expected = 7;
+		int actual = Operations.add(2, 5);
+		
+		Assertions.assertEquals(expected, actual);
+		
+	}
+}
