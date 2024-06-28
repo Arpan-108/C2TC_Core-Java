0a7e6acaca14bbb37b256a4118d7d4d72413d1c3
DELL <arpanshah848@gmail.com>
2024-06-28 13:02:16 +0530

    33.2nd Commit

--- TNSCoreJava/src/com/tnsif/testcases/testing/TestClassTwo.java
+++ TNSCoreJava/src/com/tnsif/testcases/testing/TestClassTwo.java
@@ -1,5 +1,6 @@
 package com.tnsif.testcases.testing;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
@@ -17,9 +18,19 @@ public class TestClassTwo {
 				()-> {Assertions.assertTrue(Operations.isPalindrome(121));});
 	}
 	
+	@TestFactory
 	public List<DynamicTest> testMethodTwo() {
 		
 		List<DynamicTest> tests=new ArrayList<DynamicTest>();
-		return null;
+		
+		tests.add(DynamicTest.dynamicTest("TestForFactorial",
+				()->{Assertions.assertEquals(24,  Operations.getFactorial(4));}));
+		tests.add(DynamicTest.dynamicTest("TestForPrime",
+				()->{Assertions.assertTrue(Operations.isPrime(3));}));
+		tests.add(DynamicTest.dynamicTest("TestForNull",
+				()->{Assertions.assertNull(null);}));
+		tests.add(DynamicTest.dynamicTest("TestForFalse",
+				()->{Assertions.assertFalse(12>50);}));
+		return tests;
 	}
 }
